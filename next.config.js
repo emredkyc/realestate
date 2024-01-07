8e5319ae4a0a602befe9b7113c78d6f19844bf1e
emredkyc <emredkyc@hotmail.com>
2024-01-08 00:29:28 +0100

    setup file and folder structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images:{
+    domains:['bayut-production.s3.eu-central-1.amazonaws.com']
+},
 }
-
-module.exports = nextConfig
